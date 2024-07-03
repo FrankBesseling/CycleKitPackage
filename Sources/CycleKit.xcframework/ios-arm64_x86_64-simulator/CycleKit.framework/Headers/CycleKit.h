e67c7c8f16b51c6b86c1a497befb1c0733a7f079
Frank Besseling <frankbeseling@me.com>
2024-07-03 14:13:34 +0200

    First commit

---
+++ Sources/CycleKit.xcframework/ios-arm64_x86_64-simulator/CycleKit.framework/Headers/CycleKit.h
@@ -0,0 +1,18 @@
+//
+//  CycleKit.h
+//  CycleKit
+//
+//  Created by Frank Besseling on 03/07/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CycleKit.
+FOUNDATION_EXPORT double CycleKitVersionNumber;
+
+//! Project version string for CycleKit.
+FOUNDATION_EXPORT const unsigned char CycleKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CycleKit/PublicHeader.h>
+
+
